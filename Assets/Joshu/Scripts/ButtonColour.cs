721542a32f46bfc84a8ed383293ed398cef7effd
Joshua <S298011@uos.ac.uk>
2025-05-01 15:17:02 +0100

    Reapply "Buttoncolour"
    This reverts commit f744bd1f0dd6a95ddf1763dbca1c3eb5d20070c5.

---
+++ Assets/Joshu/Scripts/ButtonColour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonColour : MonoBehaviour
+{
+
+    public ColourByNumber Number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+//variable to store random range from the start to end of array
+//pass the index value to buttons colour renderer 
+// store values until scene resets or we leave
+
+// randomise colours in index
+// set button colour to random colour 
