274f42c7b21b82ee4ea5082fd4cb646ccec01ffe
S286421 <s286421@uos.ac.uk>
2025-05-07 20:26:04 +0100

    Started to add heart effect to turtles
    About 80% there, will ask for help from team tomorrow

---
+++ Assets/Ethan/Scripts/TurtleHearts.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleHearts : MonoBehaviour
+{
+    Vector3 pos;
+    public float speed = 1f;
+
+    public SpriteRenderer spriteRenderer;
+    public bool turtleCheck;
+
+    void Start()
+    {
+        spriteRenderer.enabled = false;
+        turtleCheck = false;
+    }
+
+    void Update()
+    {
+        pos = Input.mousePosition;
+        pos.z = speed;
+        transform.position = Camera.main.ScreenToWorldPoint(pos);
+        if(Input.GetMouseButtonDown(0) && turtleCheck)
+        {
+            StartCoroutine(Visibility());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Turtle"))
+        {
+            turtleCheck = true;
+        }
+    }
+
+    IEnumerator Visibility()
+    {
+        spriteRenderer.enabled = true;
+        yield return new WaitForSeconds(1);
+        spriteRenderer.enabled = false;
+    }
+}
